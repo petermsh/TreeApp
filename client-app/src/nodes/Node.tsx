dd150389e819f85f6b2f4f289025875c83b12c98
petermsh <aron17101@gmail.com>
2024-04-08 21:20:45 +0200

    Add validation for folder name

--- client-app/src/nodes/Node.tsx
+++ client-app/src/nodes/Node.tsx
@@ -26,7 +26,7 @@ const Node = ({ node, isOpenedAll, refreshNodes, isReversed, handleDragStart, dr
     const [newChildName, setNewChildName] = useState('');
     const [editing, setEditing] = useState(false);
     const [editedName, setEditedName] = useState(node.name);
-    
+    const [nameError, setNameError] = useState('');
 
     useEffect(() => {
         const handleClick = () => setClicked(false);
@@ -47,30 +47,36 @@ const Node = ({ node, isOpenedAll, refreshNodes, isReversed, handleDragStart, dr
 
     const handleAddChildNode = async () => {
         try {
+            if (!validateName(newChildName)) {
+                setNameError('Nazwa może zawierać tylko litery, cyfry, spacje i znaki _');
+                return;
+            }
             const newNode: CreateNodeDto = { name: newChildName, parentNodeId: node.id };
             await agent.Nodes.create(newNode);
             setAddingChild(false);
-            setNewChildName(''); 
+            setNewChildName('');
+            setNameError('');
             refreshNodes(isReversed);
         } catch (error) {
             console.error('Error adding node:', error);
         }
     };
-    
-    const handleEditNode = async () => {
-        setEditing(true);
-    }
 
     const handleSaveEditedNode = async () => {
         try {
+            if (!validateName(editedName)) {
+                setNameError('Nazwa może zawierać tylko litery, cyfry, spacje i znaki _');
+                return;
+            }
             await agent.Nodes.edit({newName: editedName, id: node.id});
             setEditing(false);
+            setNameError('');
             refreshNodes(isReversed);
         } catch (error) {
             console.error('Error updating node:', error);
         }
     };
-    
+
     const handleDeleteNode = async () => {
         try {
             await agent.Nodes.delete(node.name);
@@ -78,15 +84,15 @@ const Node = ({ node, isOpenedAll, refreshNodes, isReversed, handleDragStart, dr
         } catch (error) {
             console.error('Error deleting node:', error);
         }
-    }
+    };
 
     const handleDrop = async (newParentNodeId : string) => {
-        await agent.Nodes.edit({id: draggedNode, parentNodeId: newParentNodeId})
+        await agent.Nodes.edit({id: draggedNode, parentNodeId: newParentNodeId});
         refreshNodes(isReversed);
     };
-    
+
     const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault(); 
+        e.preventDefault();
     };
 
     useEffect(() => {
@@ -94,17 +100,22 @@ const Node = ({ node, isOpenedAll, refreshNodes, isReversed, handleDragStart, dr
             if (e.key === 'Enter' && addingChild && newChildName) {
                 handleAddChildNode();
             }
-            
+
             if(e.key === 'Enter' && editing && editedName) {
                 handleSaveEditedNode();
             }
         };
-        
+
         window.addEventListener('keypress', handleKeyPress);
         return () => {
             window.removeEventListener('keypress', handleKeyPress);
         };
     }, [addingChild, newChildName, editedName, editing]);
+
+    const validateName = (name: string) => {
+        const regex = /^[a-zA-Z0-9_ ]+$/;
+        return regex.test(name);
+    };
 
     return (
         <div className={"button-container"}>
@@ -120,20 +131,24 @@ const Node = ({ node, isOpenedAll, refreshNodes, isReversed, handleDragStart, dr
             >
                 {isOpened || isOpenedAll ? <Icon name="folder open" /> : <Icon name="folder" />}
                 {editing ? (
-                    <input
-                        type="text"
-                        value={editedName}
-                        onChange={(e) => setEditedName(e.target.value)}
-                        onBlur={handleSaveEditedNode}
-                    />
+                    <>
+                        <input
+                            type="text"
+                            value={editedName}
+                            onChange={(e) => setEditedName(e.target.value)}
+                            onBlur={handleSaveEditedNode}
+                        />
+                        <div style={{color: 'red'}}>{nameError}</div>
+                    </>
+
                 ) : (
                     <span>{node.name}</span>
                 )}
             </Button>
             {clicked && (
-                <Segment top={points.y} left={points.x} style={{width: "auto" }}>
+                <Segment top={points.y} left={points.x} style={{width: "auto"}}>
                     <Button icon onClick={() => setAddingChild(true)}><Icon name="plus" /></Button>
-                    <Button icon onClick={handleEditNode}><Icon name="edit" /></Button>
+                    <Button icon onClick={() => setEditing(true)}><Icon name="edit" /></Button>
                     <Button icon onClick={handleDeleteNode}><Icon name="trash" /></Button>
                 </Segment>
             )}
@@ -143,8 +158,9 @@ const Node = ({ node, isOpenedAll, refreshNodes, isReversed, handleDragStart, dr
                         type="text"
                         value={newChildName}
                         onChange={(e) => setNewChildName(e.target.value)}
-                        placeholder="Nazwa nowego węzła"
+                        placeholder="Nazwa"
                     />
+                    <div style={{ color: 'red' }}>{nameError}</div>
                 </div>
             )}
             {node.childrens && (isOpened || isOpenedAll) && (
@@ -163,6 +179,5 @@ const Node = ({ node, isOpenedAll, refreshNodes, isReversed, handleDragStart, dr
         </div>
     );
 };
-
 export default Node;
 
